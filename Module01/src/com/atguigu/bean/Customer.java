8824bd64058f6196e9c362f335d27973e00e6596
gphgjcmqgg <gphgjcmqgg@sina.com>
2018-07-20 18:32:12 +0800

    Initial commit

---
+++ Module01/src/com/atguigu/bean/Customer.java
@@ -0,0 +1,18 @@
+package com.atguigu.bean;
+
+
+import sun.security.provider.PolicySpiFile;
+
+public class Customer {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+
+
+        if (args != null) {
+            
+        }
+
+    }
+
+    public static void a(){}
+}
---
+++ src/com/atguigu/java/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.atguigu.java;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class HelloWorld {
+
+
+    public static void main(String[] args){
+        System.out.println("HelloWorld");
+
+
+    }
+}
